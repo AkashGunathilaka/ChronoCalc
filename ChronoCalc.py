8391610eabe4852fb8d4e981cccfd96eeda20532
AKASHWG <p2783610@my365.dmu.ac.uk>
2025-04-23 12:36:34 +0100

    Update ChronoCalc.py

--- ChronoCalc.py
+++ ChronoCalc.py
@@ -1,4 +1,5 @@
 import datetime
+from dateutil.relativedelta import relativedelta
 def selector():
     # This function is used to select the function to be executed before i had the function call on itself and realized this leads to stack overflow so i changed it into a while true loop 
     while True:
@@ -7,7 +8,7 @@ Select the function you want to execute:
 1. Time to current date
 2. Time between two dates
 3. Time to future date             
-3. Exit""")
+4. Exit""")
 
         choice = input("Enter your choice: ")
 
@@ -18,17 +19,15 @@ Select the function you want to execute:
         elif choice == '3':
             timetofuture()    
         elif choice == '4':
-            exit()
+            print("Thanks for using ChronoCalc. Goodbye!")
+            break # use break instead of exit for graceful termination ie graceful termination means that the program will close all resources when exiting the app and close connections without immediately ending the program
         else:
             print("Invalid choice. Please try again.")
-            break
+            
 
 def timetocurrent():
     # This function calculates the time difference to the current date 
-    date1 = input("Enter the date (YYYY/MM/DD): ")
-    
-    # Convert input date to a datetime object
-    date1 = datetime.datetime.strptime(date1, "%Y/%m/%d")
+    date1 = get_valid_date("Enter the date (YYYY/MM/DD): ")
     
     # Get current date as a datetime object
     currentdate = datetime.datetime.now()
@@ -41,6 +40,7 @@ def timetocurrent():
     # Adjust if current day is earlier in the month than the input day
     if days < 0:
         months -= 1
+        prev_month = (currentdate.month - 1) or 12
         prev_year = currentdate.year if currentdate.month != 1 else currentdate.year - 1
         days_in_prev_month = (datetime.datetime(prev_year, prev_month + 1, 1) - datetime.datetime(prev_year, prev_month, 1)).days
         days += days_in_prev_month
@@ -52,13 +52,11 @@ def timetocurrent():
 
     print(f"The difference is: {years} years, {months} months, and {days} days.")
 
-    selector()
+    
 
 def timetofuture():
     # This function calculates the time difference to the future date
-    date1 = input("Enter the date (YYYY/MM/DD): ")
-    
-    date1 = datetime.datetime.strptime(date1, "%Y/%m/%d")
+    date1 = get_valid_date("Enter the date (YYYY/MM/DD): ")
 
     currentdate = datetime.datetime.now()
 
@@ -81,42 +79,48 @@ def timetofuture():
         months += 12
 
     print(f"The difference is: {years} years, {months} months, and {days} days.")
-    selector()
+    
 
 
 def timebetween():
     # This function calculats the time between two dates
-    date1 = input("Enter the first date (YYYY/MM/DD H:M): ")
-    date2 = input("Enter the second date (YYYY/MM/DD H:M): ")
+    date1 = get_valid_date("Enter the first date (YYYY/MM/DD H:M): ", with_time=True)
+    date2 = get_valid_date("Enter the second date (YYYY/MM/DD H:M): ", with_time=True)
 
     # so in this the user enters the date but it is stored as a string and we need to convert it into a date object to do this we need to parse the string and get the relevant information
 
-    try :
-        if " " in date1 and " " in date2:
-            date1 = datetime.datetime.strptime(date1, "%Y/%m/%d %H:%M")
-            date2 = datetime.datetime.strptime(date2, "%Y/%m/%d %H:%M")
-        else:
-            date1 = datetime.datetime.strptime(date1, "%Y/%m/%d")
-            date2 = datetime.datetime.strptime(date2, "%Y/%m/%d")
-
-    except ValueError:
-        print("Invalid date format. Please enter the date in the correct format.")
-        timebetween()
-        return        
-
     # Now we can calculate the difference between the two dates
 
-    difference = date2 - date1
-    days = difference.days
-    hours = difference.seconds // 3600  
-    minutes = (difference.seconds // 60) % 60
+    if date1 > date2:
+        date1, date2 = date2, date1
+    
+    difference = relativedelta(date2, date1)
+
+
+    
     # Now we can print the difference
-    print(f"The difference is: {days} days, {hours} hours, and {minutes} minutes.")
-    selector()
+    print(f"The difference is: {difference.years} years, {difference.days} days, {difference.hours} hours, and {difference.minutes} minutes.")
+
+def get_valid_date(prompt , with_time = False):
+    # This function is used to get a valid date from the user
+    while True:
+        user_input = input(prompt).strip()  # Remove leading and trailing whitespace
+
+        if with_time:
+
+            if " " not in user_input:
+                user_input += " 00:00"
+
+        date_format = "%Y/%m/%d %H:%M" if with_time else "%Y/%m/%d"
+        
+        try:
+            return datetime.datetime.strptime(user_input, date_format)
+        except ValueError:
+            readable_date_format = "YYYY/MM/DD" if not with_time else "YYYY/MM/DD HH:M<"
+            print(f"Invalid date format. Please enter the date in the format {readable_date_format}.")
+
+
     
 # when we subtract two date objects we get a timedelta object which is not clear so we need to convert it into a more readable format by breaking down the time delta object and labelling the different parts our self and printing a formatted string 
 selector()
 
-
-# now we need to do some error handling to make sure the user enters in the correct format
-# we can also write a function to handle if the month is later in the year than the input month
